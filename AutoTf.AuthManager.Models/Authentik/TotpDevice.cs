c033f1f052ebb976e02cdc6be636efdf9650b6ae
chiouya <chiouyazo@gmail.com>
2025-04-26 17:20:35 +0200

    Applied split to wrong string

--- AutoTf.AuthManager.Models/Authentik/TotpDevice.cs
+++ AutoTf.AuthManager.Models/Authentik/TotpDevice.cs
@@ -44,11 +44,11 @@ public class TotpDevice
         {
             VerboseName = item.GetProperty("verbose_name").GetString() ?? "Unknown",
             VerboseNamePlural = item.GetProperty("verbose_name_plural").GetString() ?? "Unknown",
-            // Maybe remove this and do that via a converted?
-            MetaModelName = item.GetProperty("meta_model_name").GetString()?.Split('.')[1] ?? "Unknown",
+            MetaModelName = item.GetProperty("meta_model_name").GetString() ?? "Unknown",
             Pk = item.GetProperty("pk").GetString() ?? "Unknown",
             Name = item.GetProperty("name").GetString() ?? "Unknown",
-            Type = item.GetProperty("type").GetString() ?? "Unknown",
+            // Maybe remove this and do that via a converted?
+            Type = item.GetProperty("type").GetString()?.Split('.')[1] ?? "Unknown",
             Confirmed = item.GetProperty("confirmed").GetBoolean(),
             Created = item.GetProperty("created").GetString() ?? "Unknown",
             LastUpdated = item.GetProperty("last_updated").GetString() ?? "Unknown",
