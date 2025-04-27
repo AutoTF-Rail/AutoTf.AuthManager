0e86552af4497be893bdb0228f40aad215829831
chiouya <chiouyazo@gmail.com>
2025-04-27 20:29:43 +0200

    Added dockerfile and publish script for no frontend

---
+++ publishNoFrontend.sh
@@ -0,0 +1,5 @@
+# This script is used on the server that hosts the auth manager.
+dotnet publish -c ReleaseNoFrontend -o out
+docker login 172.17.0.3:5001
+docker build -t 172.17.0.3:5001/repository/docker-autotf/autotf.AuthentikDashboard:latest ./DockerfileNoFrontend
+docker push 172.17.0.3:5001/repository/docker-autotf/autotf.AuthentikDashboard:latest
