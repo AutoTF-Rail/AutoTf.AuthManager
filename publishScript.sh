dbc8dc4a91bd34dc8e14d54c26e2f39896ac9c96
chiouya <chiouyazo@gmail.com>
2025-04-27 20:31:27 +0200

    Fixed docker capitilisation

--- publishScript.sh
+++ publishScript.sh
@@ -1,5 +1,5 @@
 # This script is used on the server that hosts the auth manager.
 dotnet publish -c Release -o out
 docker login 172.17.0.3:5001
-docker build -t 172.17.0.3:5001/repository/docker-autotf/autotf.AuthentikDashboard:latest .
-docker push 172.17.0.3:5001/repository/docker-autotf/autotf.AuthentikDashboard:latest
+docker build -t 172.17.0.3:5001/repository/docker-autotf/autotf.authentikdashboard:latest .
+docker push 172.17.0.3:5001/repository/docker-autotf/autotf.authentikdashboard:latest
