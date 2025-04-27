3e6c110dd2af68416a8aad2f8b3ab3d9a9b2c0b7
chiouya <chiouyazo@gmail.com>
2025-04-27 20:24:01 +0200

    Added userscontroller/me endpoint

---
+++ AutoTf.AuthentikDashboard/Controllers/Core/UsersController.cs
@@ -0,0 +1,17 @@
+using AutoTf.AuthentikDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoTf.AuthentikDashboard.Controllers.Core;
+
+[ApiController]
+[Route("core/users")]
+public class UsersController : AuthentikController
+{
+    public UsersController(UserManager userManager) : base(userManager) { }
+    
+    [HttpGet("me")]
+    public async Task<ActionResult<string>> Me()
+    {
+        return await HttpHelper.SendGetString("/api/v3/core/users/me/");
+    }
+}
