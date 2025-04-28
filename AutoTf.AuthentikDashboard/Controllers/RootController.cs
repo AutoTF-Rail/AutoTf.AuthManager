5c9933b239e54bab4a742687b613fe5f263bc3c7
chiouya <chiouyazo@gmail.com>
2025-04-28 12:35:53 +0200

    Added token endpoints

--- AutoTf.AuthentikDashboard/Controllers/RootController.cs
+++ AutoTf.AuthentikDashboard/Controllers/RootController.cs
@@ -11,4 +11,26 @@ public class RootController : ControllerBase
     {
         return Ok();
     }
+	
+    [HttpGet("/token")]
+    public IActionResult Token()
+    {
+        return File("~/token.html", "text/html");
+    }
+	
+    [HttpGet("/tokenStep")]
+    public IActionResult TokenStep()
+    {
+        string? csrfToken = Request.Query["csrf_token"];
+
+        KeyValuePair<string, string> proxyToken = Request.Cookies.FirstOrDefault(c => c.Key.StartsWith("authentik_proxy"));
+
+        if (!string.IsNullOrEmpty(csrfToken) && !string.IsNullOrEmpty(proxyToken.Key))
+        {
+            string redirectUrl = $"http://localhost:5000/token?csrf_token={csrfToken}&proxy_name={proxyToken.Key}&proxy_token={proxyToken}";
+            return Redirect(redirectUrl);
+        }
+
+        return Content("Tokens not found.");
+    }
 }
